3adc76ef5abbc66672720730eb22abdb4a1b1049
Vikas Garg <vikas04522@gmail.com>
2019-08-25 15:12:58 -0400

    Updated dependecies, added navigation for Profile and Profile related files

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -211,7 +211,7 @@ class Home extends Component{
   }
 
   navigateToAccount = () =>{
-    // this.props.history.push('/account');
+    this.props.history.push('/profile');
   }
 }
 
@@ -329,6 +329,9 @@ class HomeItem extends Component{
   }
 
   onAddCommentClicked = (id) => {
+    if (this.state.comment === "" || typeof this.state.comment === undefined) {
+      return;
+    }
     this.setState({
       comment:""
     });
